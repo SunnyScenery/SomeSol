[MOD] finish basic zhang's method, wrong results remained fixed

zhang/calibration.py
@@ -2,10 +2,18 @@ import numpy as np
 from dataloader import *
 from homography import *
 from intrinsic import *
+from extrinsic import *
 import sys
+import argparse
+
+# argument
+parser = argparse.ArgumentParser()
+parser.add_argument("--imgpath", type=str, default='/Users/scenery/Code/SomeGit/Stereo/Problems/stereo/', help="path to the origin image")
+opt = parser.parse_args()
 
 # load the image data
-objpoints, imgpoints = dataloader()
+objpoints, imgpoints = dataloader(root=opt.imgpath)
+
 # delete the axis full of zero
 for i in range(len(objpoints)):
     objpoints[i] = np.delete(objpoints[i],2,axis=1)
@@ -19,10 +27,28 @@ for i in range(len(objpoints)):
 # get the camera matrix
 cameraMatrix = get_intrinsic(Hs)
 
-# print information
+# print camera matrix information
 sys.stdout.write(
-    "[%f, %f, %f]\n[%f, %f, %f]\n[%f, %f, %f]\n"
+    "the camera matrix is: \n[%f, %f, %f]\n[%f, %f, %f]\n[%f, %f, %f]\n"
     % (cameraMatrix[0][0], cameraMatrix[0][1], cameraMatrix[0][2], 
        cameraMatrix[1][0], cameraMatrix[1][1], cameraMatrix[1][2], 
        cameraMatrix[2][0], cameraMatrix[2][1], cameraMatrix[2][2])
+    )
+
+# calculate the extrinsic
+# currently only test the first view to judge the correctness
+lambda_0, R, T = get_extrinsic(Hs[0], cameraMatrix)
+
+# print extrinsic information
+sys.stdout.write(
+    "the rotation matrix is: \n[%f, %f, %f]\n[%f, %f, %f]\n[%f, %f, %f]\n"
+    % (R[0][0], R[0][1], R[0][2], 
+       R[1][0], R[1][1], R[1][2], 
+       R[2][0], R[2][1], R[2][2])
+    )
+sys.stdout.write(
+    "the lambda is: %f\n" % lambda_0)
+sys.stdout.write(
+    "the translation vector is: \n[%f, %f, %f]\n"
+    % (T[0], T[1], T[2])
     )

zhang/intrinsic.py
@@ -3,6 +3,7 @@ import math
 
 def get_intrinsic(Hs):
     M_views = len(Hs)
+    # make the matrix V in V*b=0 equation
     V = np.zeros((2 * M_views, 6))
     for i in range(M_views):
         H = Hs[i]
@@ -27,20 +28,24 @@ def get_intrinsic(Hs):
                                  H[2][1] * H[1][1] + H[1][1] * H[2][1],
                                  H[2][1] * H[2][1],
                              ]))
+
+    # svd solution
     u, s, vh = np.linalg.svd(V)
     k = np.argmin(s)
     B = vh[k]
 
+    # closed-form solution
     w = B[0] * B[2] * B[5] - B[1] * B[1] * B[5] - B[0] * B[4] * B[4] + 2 * B[1] * B[3] * B[4] - B[2] * B[3] * B[3]
     d = B[0] * B[2] - B[1] * B[1]
 
     alpha = math.sqrt(w / (d * B[0]))
     beta = math.sqrt(w * B[0] / (d ** 2))
-    yita = B[1] * math.sqrt(w / ((d ** 2) * B[0]))
+    gamma = B[1] * math.sqrt(w / ((d ** 2) * B[0]))
     u_c = (B[1] * B[4] - B[2] * B[3]) / d
     v_c = (B[1] * B[3] - B[0] * B[4]) / d
 
-    cameraM = np.array([[alpha, yita, u_c],
+    # get the final cameraMatrix
+    cameraM = np.array([[alpha, gamma, u_c],
                         [0.   , beta, v_c],
                         [0.   , 0.  , 1. ]])
 